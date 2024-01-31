bea24a4ca04e10048d44bd78c31657c6844e95c1
Edson <120113548+WillCossBr@users.noreply.github.com>
2024-01-31 08:30:15 -0300

    Add files via upload

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,87 @@
+#----------------------------------------------------------------
+#Operadores Aritméticos
+
+# ** = Potênciação
+# * = Multiplicação
+# / = Divisão
+# + = soma
+# - = subtração
+
+#----------------------------------------------------------------
+#Ordem dos operadores aritméticos
+
+# 1º parenteses ()
+# 2º potênciação **
+# 3º multiplicação e divisão * e /
+# 4º adição e subtração + e -
+
+#----------------------------------------------------------------
+#Casting
+#Converte o tipo de uma variável por outro.
+
+#exemplo - 1 adição
+
+print('-'*30)
+print("Exemplo - 1 adição")
+x = int(input("Digite um número: "))
+y = int(input("Digite um número: "))
+z = 0
+
+z = x + y
+
+print("A soma dos dois valores é: ", z)
+print('-'*30)
+
+#----------------------------------------------------------------
+#exemplo - 2 subtração
+
+print("Exemplo - 2 subtração")
+x1 = int(input("Digite um número: "))
+y1 = int(input("Digite um número: "))
+z1 = 0
+
+z1 = x1 - y1
+
+print("A subtração dos dois valores é: ", z1)
+print('-'*30)
+
+#----------------------------------------------------------------
+#exemplo - 3 multiplicação
+
+print("Exemplo - 3 multiplicação")
+x2 = int(input("Digite um número: "))
+y2 = int(input("Digite um número: "))
+z2 = 0
+
+z2 = x2 * y2
+
+print("A multiplicação dos dois valores é: ", z2)
+print('-'*30)
+
+#----------------------------------------------------------------
+#exemplo - 4 divisão
+
+print("Exemplo - 3 divisão")
+x3 = int(input("Digite um número: "))
+y3 = int(input("Digite um número: "))
+z3 = 0
+
+z3 = x3 / y3
+
+print("A divisão dos dois valores é: ", z3)
+print('-'*30)
+
+#----------------------------------------------------------------
+#exemplo - 5 potênciação
+
+print("Exemplo - 3 potênciação")
+x4 = int(input("Digite um número: "))
+y4 = int(input("Digite um número: "))
+z4 = 0
+
+z4 = x4 ** y4
+
+print("A potênciação dos dois valores é: ", z4)
+print('-'*30)
+
+#----------------------------------------------------------------
