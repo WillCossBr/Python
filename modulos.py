410580f86c368cea90f9cfe007331307e32113d0
Edson <120113548+WillCossBr@users.noreply.github.com>
2024-02-04 18:36:12 -0300

    Add files via upload

---
+++ modulos.py
@@ -0,0 +1,26 @@
+#----------------------------------------------------------------
+#módulos
+
+import math
+
+#extrair raiz quadrada
+print(math.sqrt(81))
+
+#----------------------------------------------------------------
+
+#import math = importa o módulo.
+
+#from math import math = importa apenas uma função.
+
+#from math import math* = importe universal(módulo completo).
+#não é recomendado, pois pode causar conflito entre outros módulos.
+
+#import math as m = as m(da um apelido ao módulo).
+
+#É possivel importar os próprios módulos.
+
+#if__name__== '__main__':
+#ponto inicial do programa serve para que o interpretador
+#não confunda os módulos importados com a função principal.
+
+#----------------------------------------------------------------
