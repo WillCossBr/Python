b9951d2621be26d8a793a3d707ec397eba19ba40
Edson <120113548+WillCossBr@users.noreply.github.com>
2024-02-22 19:54:36 -0300

    Add files via upload

---
+++ Manipulação_de_arquivos/Json/escrever_json.py
@@ -0,0 +1,35 @@
+#escrever arquivos em json:
+
+print(30*'-')
+print('Escrever em arquivos json: ')
+
+#importa o módulo
+import json
+
+#dados que deseja escrever
+dados = {
+    "nome": "Danones.LTDA",
+    "telefone": "(55) 3333-3333",
+    "email": "danones.ltda@gmail.com",
+    "endereco": "Rua do limoeiro, 123, Bairro: Monica",
+}
+
+#endereço do arquivo que deseja editar
+local = "/home/william/Python/Manipula_Arqs/fornecedores.json"
+
+#editar o arquivo:
+with open(local, 'w') as fornecedores:
+    json.dump(dados, fornecedores)
+
+#Confirmação de deu certo!
+print('Dados inseridos com sucesso!')
+
+print(30*'-')
+
+#impressão do arquivo:
+with open(local, 'r') as fornecedores:
+    leitor = json.load(fornecedores)
+    print(json.dumps(leitor ,indent=4))
+
+
+print('-'*30)
